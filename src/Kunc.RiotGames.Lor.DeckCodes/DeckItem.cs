477756ddb7491b79e450e117bcf8ac248ee4ce1a
AoshiW <ondru@LAPTOP-7K2AEST8>
2024-11-20 11:07:38 +0100

    remove `==`, `!=` operators

--- src/Kunc.RiotGames.Lor.DeckCodes/DeckItem.cs
+++ src/Kunc.RiotGames.Lor.DeckCodes/DeckItem.cs
@@ -52,22 +52,4 @@ public class DeckItem : IReadOnlyDeckItem, IDeckItem, IEquatable<DeckItem?>
     {
         return HashCode.Combine(CardCode, Count);
     }
-
-    /// <summary>Compares two values to determine equality.</summary>
-    /// <param name="left">The value to compare with <paramref name="right" />.</param>
-    /// <param name="right">The value to compare with <paramref name="left" />.</param>
-    /// <returns><c>true</c> if <paramref name="left" /> is equal to <paramref name="right" />; otherwise, <c>false</c>.</returns>
-    public static bool operator ==(DeckItem? left, DeckItem? right)
-    {
-        return EqualityComparer<DeckItem>.Default.Equals(left, right);
-    }
-
-    /// <summary>Compares two values to determine inequality.</summary>
-    /// <param name="left">The value to compare with <paramref name="right" />.</param>
-    /// <param name="right">The value to compare with <paramref name="left" />.</param>
-    /// <returns><c>true</c> if <paramref name="left" /> is not equal to <paramref name="right" />; otherwise, <c>false</c>.</returns>
-    public static bool operator !=(DeckItem? left, DeckItem? right)
-    {
-        return !(left == right);
-    }
 }

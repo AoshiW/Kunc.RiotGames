14e917d28fdeb0499349929fb02bc16385a99980
AoshiW <Aoshi.W@gmail.com>
2025-02-11 09:03:27 +0100

    update dependencies

--- example/NotExample/Program.Partial.cs
+++ example/NotExample/Program.Partial.cs
@@ -8,7 +8,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using NeoSmart.Caching.Sqlite;
-using NeoSmart.Caching.Sqlite.AspNetCore;
 
 partial class Program
 {

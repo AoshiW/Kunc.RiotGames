87bf44fc64c1eb59d9fb8656fd627e0fa8ecddb3
AoshiW <Aoshi.W@gmail.com>
2025-06-23 20:22:00 +0200

    remove test for `GetSummonerBySummonerIdAsync`

--- tests/Kunc.RiotGames.Api.Tests/TftSummonerV1Test.cs
+++ tests/Kunc.RiotGames.Api.Tests/TftSummonerV1Test.cs
@@ -15,15 +15,4 @@ public class TftSummonerV1Test : ApiBase<TGame.TFT>
         Assert.IsNotNull(summoner);
         Assert.IsTrue(summoner.RevisionDate > DateTimeOffset.UtcNow.AddMonths(-2), "The account has been inactive for more than 2 months.");
     }
-
-    [TestMethod]
-    public async Task GetSummonerBySummonerIdAsync()
-    {
-        var acc = GetConfiguration("Summoner").Get<AccountInfo>()!;
-        var summonerId = GetConfiguration("Summoner:Id").Get<string>()!;
-
-        var summoner = await Api.TftSummonerV1.GetSummonerBySummonerIdAsync(acc.Region, summonerId);
-
-        Assert.IsNotNull(summoner);
-    }
 }
